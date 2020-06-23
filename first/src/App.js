5dc77b4bc9bb970e3fb52e84158a7b2a5ca7c4aa
Harshita-Kanal <harshita.kgv@gmail.com>
2020-06-24 02:15:53 +0530

    Added MenuComponent

--- first/src/App.js
+++ first/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import Menu from "./components/MenuComponents";
 import logo from './logo.svg';
 import  {Navbar , NavbarBrand} from 'reactstrap';
 import './App.css';
@@ -7,12 +7,13 @@ import './App.css';
 class App extends Component {
   render(){
     return(
-      <div className = "App">
+      <div >
        <Navbar dark color="primary">
           <div className = "container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
        </Navbar>
+       <Menu />
       </div>
     );
 
