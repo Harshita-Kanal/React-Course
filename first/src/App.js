04ab5608fd2e93489051eaf399fbfae5166e6272
Harshita-Kanal <harshita.kgv@gmail.com>
2020-06-24 18:20:04 +0530

    Added Card Components

--- first/src/App.js
+++ first/src/App.js
@@ -2,9 +2,17 @@ import React, {Component} from 'react';
 import Menu from "./components/MenuComponents";
 import logo from './logo.svg';
 import  {Navbar , NavbarBrand} from 'reactstrap';
+import { DISHES } from './shared/dishes'
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES
+    }
+  }
   render(){
     return(
       <div >
@@ -13,7 +21,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
        </Navbar>
-       <Menu />
+       <Menu  dishes = {this.state.dishes}/>
       </div>
     );
 
