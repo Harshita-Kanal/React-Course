9babb586b66506561970efc2da28a63b65c399c3
Harshita-Kanal <harshita.kgv@gmail.com>
2020-06-30 05:34:56 +0530

    Added About Page

--- first/src/components/MainComponent.js
+++ first/src/components/MainComponent.js
@@ -7,6 +7,7 @@ import DishDetail from "./DishdetailComponent";
 import Home from './HomeComponent';
 import Contact from './ContactComponent'
 import Footer from "./FooterComponent";
+import About from './AboutComponent'
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS} from '../shared/promotions';
@@ -60,6 +61,7 @@ class Main extends Component {
           <Route path = '/home' component = {HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
           <Route exact path="/contactus"  component = {Contact}/>
+          <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
           <Route path = '/menu/:dishId' component = {DishWithId} />
           <Redirect to = "/home" />
        </Switch>
